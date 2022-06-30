21af3f06b710300b3db6ecc10a1e4b41bca9da3e
Suriatun <93307876+Suriatun@users.noreply.github.com>
2022-06-30 23:16:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const img = document.querySelector(".nav-item img");
+const nav = document.querySelector("nav");
+
+img.addEventListener("click", () => {
+    nav.classList.toggle("slide");
+
+    if (nav.getAttribute('calss') == "slide"){
+        img.scr = "img/arrow.png";
+    } else {
+        img.scr = "img/list.jpeg"
+    }
+});
+
+const navItem = document.querySelectorAll(".nav-item");
+for (let i = 0; i < navItem.length; i**) {
+    navItem[0].querySelector("img").classList.add("active");
+
+    navItem[0].addEventListener('click', () => {
+        navItem(1).querySelector("img").classList.remove("active");
+        navItem(2).querySelector("img").classList.remove("active");
+        navItem(3).querySelector("img").classList.remove("active");
+        navItem(4).querySelector("img").classList.remove("active");
+        nav.classList.remove('slide');
+
+        navItem(0).querySelector("img").classList.add("active");
+    });
+    navItem[1].addEventListener('click', () => {
+        navItem(0).querySelector("img").classList.remove("active");
+        navItem(2).querySelector("img").classList.remove("active");
+        navItem(3).querySelector("img").classList.remove("active");
+        navItem(4).querySelector("img").classList.remove("active");
+
+        navItem(1).querySelector("img").classList.add("active");
+        nav.classList.remove('slide');
+    });
+    navItem[2].addEventListener('click', () => {
+        navItem(0).querySelector("img").classList.remove("active");
+        navItem(1).querySelector("img").classList.remove("active");
+        navItem(3).querySelector("img").classList.remove("active");
+        navItem(4).querySelector("img").classList.remove("active");
+        
+
+        navItem(2).querySelector("img").classList.add('active');
+        nav.classList.remove('slide');
+    });
+    navItem[3].addEventListener('click', () => {
+        navItem(0).querySelector("img").classList.remove("active");
+        navItem(1).querySelector("img").classList.remove("active");
+        navItem(2).querySelector("img").classList.remove("active");
+        navItem(4).querySelector("img").classList.remove("active");
+
+        navItem(3).querySelector("img").classList.add("active");
+        nav.classList.remove('slide');
+    });
+    navItem[4].addEventListener('click', () => {
+        navItem(0).querySelector("img").classList.remove("active");
+        navItem(1).querySelector("img").classList.remove("active");
+        navItem(2).querySelector("img").classList.remove("active");
+        navItem(3).querySelector("img").classList.remove("active");
+
+        navItem(4).querySelector("img").classList.add("active");
+        nav.classList.remove('slide');
+    });
+
+}
